d0045f123a0166335358ad93aa5f196b699855da
vinaykumariet <singhalvinayiet@gmail.com>
2019-04-04 09:11:20 +0545

    update with more programme

--- test.py
+++ test.py
@@ -1,3 +1,108 @@
+def reverse(string):
+    print(string)
+    print(len(string))
+    if len(string) == 0:
+        return string
+    else:
+        return reverse(string[1:]) + string[0]
+a = str(input("Enter the string to be reversed: "))
+print(reverse(a))
+
+def sortf(str):
+  k=str
+  li = []
+  x = len(k)
+  for i in range (0,x):
+    li.append(k[i])
+  for i in range(0,x):
+    for j in range(0,x):
+        if li[i]<li[j]:
+            temp = li[i]
+            li[i]=li[j]
+            li[j]=temp
+  j= " ".join(li)
+  # for i in range(0,x):
+  #   j+= li[i]
+
+  return j  
+
+
+
+
+# Python program to check whether two strings are  
+# anagrams of each other  
+  
+# function to check whether two strings are anagram  
+# of each other  
+def areAnagram(str1, str2):  
+    # Get lengths of both strings  
+    n1 = len(str1)  
+    n2 = len(str2)  
+  
+    # If lenght of both strings is not same, then  
+    # they cannot be anagram  
+    if n1 != n2:  
+        return 0
+  
+    # Sort both strings  
+    str1 = sortf(str1) 
+    str2 = sortf(str2) 
+  
+    # Compare sorted strings  
+    for i in range(0, n1):  
+        if str1[i] != str2[i]:  
+            return 0
+  
+    return 1
+  
+  
+# Driver program to test the above function  
+str1 = "test"
+str2 = "tste"
+# if areAnagram(str1, str2):  
+#     print ("The two strings are anagram of each other") 
+# else:  
+#     print ("The two strings are not anagram of each other") 
+  
+# This code is contributed by Bhavya Jain  
+
+
+
+
+
+
+# Program to display the Fibonacci sequence up to n-th term where n is provided by the user
+
+# change this value for a different result
+nterms = 10
+
+# uncomment to take input from the user
+#nterms = int(input("How many terms? "))
+
+# first two terms
+n1 = 0
+n2 = 1
+count = 0
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
+else:
+   #print("Fibonacci sequence upto",nterms,":")
+   while count < nterms:
+       #print(n1,end=' , ')
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
+
+
+
+
 var ="gg.f.fk"
 
 i=0
@@ -42,7 +147,7 @@ for i in range(0,x):
 j=""
 
 for i in range(0,x):
-    j = j+li[i]
+    j+= li[i]
 
 #print("After sorting String is : ",j)
 
@@ -106,6 +211,6 @@ def Findduplicate(n):
   return duplicate_char  
 
 
-print(Findduplicate('Great responsibility'))
+# print(Findduplicate('Great responsibility'))
 
 
